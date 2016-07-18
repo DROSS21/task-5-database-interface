45bd9e80f32030ffad5fee841dc1bc09ea9744eb
dariseejr <dariseejr@gmail.com>
2016-07-18 13:19:25 -0400

    Updated script

--- questions.sh
+++ questions.sh
@@ -22,6 +22,10 @@ ANSWERS="$ans1,$ans2,$ans3,$ans4,$ans5,$DATE"
 echo $ANSWERS >> eureka.csv
 
 mysql -u root -p << EOF
+CREATE DATABASE IF NOT EXISTS questionnaire;
 USE questionnaire;
+CREATE TABLE IF NOT EXISTS questionnaire (Username VARCHAR(15), Distance DECIMAL(5,2), Year INT, Commute VARCHAR(3), Learned VARCHAR(3), Date VARCHAR(30), PRIMARY KEY (Date));
+CREATE TEMPORARY TABLE temporary_table LIKE questionnaire;
+LOAD DATA INFILE '/home/cabox/workspace/task-5-database-interface/eureka.csv' INTO TABLE temporary_table FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"';
 LOAD DATA INFILE '/home/cabox/workspace/task-5-database-interface/eureka.csv' INTO TABLE questionnaire FIELDS TERMINATED BY ',' OPTIONALLY ENCLOSED BY '"';
 EOF
